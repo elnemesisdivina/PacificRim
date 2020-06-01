a3b7fa1e4020df61784ef005d0d18a55ad28e8c5
+vRay <elnemesisdivina@gmail.com>
2020-06-01 11:39:40 -0500

    Create dplyctnrnative.sh

---
+++ dplyctnrnative.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+########################
+include the magic
+########################
+
+. ./demo-magic.sh
+
+clear
+
+DEMO_PROMPT="ubuntu@cli-vm-corp-local:~/scripts$ "
+
+pe "kubectl vsphere login --server 192.168.124.1 -u vray@vsphere.local"
+pe "kubectl config get-contexts" #kubectx where is supported
+pe "kuberctl config use-context satdemo"
+pe "cat /scripts/nginx-deployment.yaml"
+pe "kubectl apply -f /root/scripts/ngnix-deployment.yaml"
+pe "kubectl get pods"
+pe "kubectl describe deployment ngnix-deployment"
+
