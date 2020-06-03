c9c8dc2f45287db061daa2e2f7411233462a41f4
+vRay <elnemesisdvina@gmail.com>
2020-06-03 16:26:49 -0500

    adding more stuff

--- ctnrontkg.sh
+++ ctnrontkg.sh
@@ -12,6 +12,7 @@ clear
 DEMO_PROMPT="ubuntu@cli-vm-corp-local:~/scripts$ "
 
 #login in native namespace
+
 #pe "kubectl vsphere login --server k8s.corp.local --vsphere-username administrator@corp.local --insecure-skip-tls-verify"
 
 #login in TKG cluster 
@@ -22,14 +23,12 @@ pe "kubectl vsphere login --server k8s.corp.local --vsphere-username administrat
 
 pe "kubectl configure use-context tkg-cluster"
 
-#apply the run as nonroot
-
 #describe the sit
 
 pe "kubectl describe managedcluster tkg-cluster"
 
 #login in the cluster TKG itself
-
+pe "kubectl vsphere login --server k8s.corp.local -u administrator@vsphere.local --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name tkg-cluster --tanzu-kubernetes-cluster-namespace tkg"
 pe "kubectl vsphere login -u administrator@corp.local --server=k8s.corp.local --tanzu-kubernetes-cluster-namespace tkg --tanzu-kubernetes-cluster-name tkg-cluster --insecure-skip-tls-verify"
 
 #change to context internal to the TKG cluster 
@@ -37,12 +36,23 @@ pe "kubectl vsphere login -u administrator@corp.local --server=k8s.corp.local --
 pe "kubectl configure use-context tkg-cluster"
 
 #check is empty
+
 pe "kubectl get pods"
 
+#download yaml of policy nonroot this is the alternative but we have it on the repo
+#wget https://github.com/mwest44/pacific/raw/master/authorize-psp-for-gc-service-accounts.yaml
+
+#apply the run as nonroot policy
+
+"kubectl apply -f allow-runasnonroot-clusterrole.yaml" 
 #create the shit
+
 pe "kubectl apply -f demo-app.yaml"
+
 #check everything is being created
+
 pe "kubectl get pods --watch"
+
 #check services
 
 pe "kubectl get services"
