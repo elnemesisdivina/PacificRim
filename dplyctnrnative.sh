61d26d6dee94b8a57e8052a9d0f27c2847c63192
+vRay <elnemesisdivina@gmail.com>
2020-06-04 16:07:24 -0500

    Update dplyctnrnative.sh

--- dplyctnrnative.sh
+++ dplyctnrnative.sh
@@ -10,9 +10,9 @@ clear
 
 DEMO_PROMPT="ubuntu@cli-vm-corp-local:~/scripts$ "
 
-pe "kubectl vsphere login --server k8s.corp.local -u vray@vsphere.local"
+pe "kubectl vsphere login --server k8s.corp.local -u administrator@vsphere.local"
 pe "kubectl config get-contexts" #kubectx where is supported
-pe "kuberctl config use-context svc"
+pe "kubectl config use-context svc"
 pe "cat nginx-deployment.yaml"
 pe "kubectl apply -f /root/scripts/ngnix-deployment.yaml"
 pe "kubectl get deployment"
